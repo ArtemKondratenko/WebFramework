31cd97dbc7a4b30e2ca74494becb122ad16c0bbd
ArtemKondratenko <artem-osteo@yandex.ru>
2025-09-05 17:36:03 +0300

    Добавлена типизация

--- src/webframework/types.py
+++ src/webframework/types.py
@@ -1,6 +1,7 @@
-from collections.abc import Callable
+from collections.abc import Callable, Awaitable
 
 from typing import Literal, Any
+
 
 type Method = Literal["GET", "POST", "DELETE", "PUT"]
 
@@ -9,3 +10,11 @@ type PathPattern = str
 type Path = str
 
 type Handler = Callable[..., Any]
+
+type Message = dict[str, Any]
+
+type Scope = Message
+
+type Receive = Callable[[], Awaitable[Message]]
+
+type Send = Callable[[Message], Awaitable[None]]
