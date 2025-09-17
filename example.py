c0ce38915403d2ae923cd23122d97fff2c4b1b2a
ArtemKondratenko <artem-osteo@yandex.ru>
2025-09-17 15:17:48 +0300

    Обнавлен способ авторизации

--- example.py
+++ example.py
@@ -6,9 +6,10 @@ from typing import Annotated, Any
 import uvicorn
 
 from webframework.application import App
-from webframework.request import body, parsed, path
+from webframework.request import body, header, parsed, path
 from webframework.response import Response
 from webframework.router import Router
+from webframework.security import BasicHTTPAuthCredentials
 
 
 class User:
@@ -30,10 +31,38 @@ db = dict[str, User]()
 router = Router()
 
 
+def get_admin(auth: BasicHTTPAuthCredentials) -> User | Response:
+    admin = db.get(auth.login)
+
+    if admin is None or admin.password != auth.password or not admin.admin:
+        return Response(
+            401,
+            json.dumps({"detail": "Admin not found or assert password"}).encode(),
+            {"Content-Type": "application/json"},
+        )
+    return admin
+
+
 @router.post("/users")
 async def create_user(
     user: Annotated[User, parsed(body(), User.from_dict)],
+    auth: Annotated[
+        BasicHTTPAuthCredentials,
+        parsed(header("Authorization"), BasicHTTPAuthCredentials.parse),
+    ],
 ) -> Response:
+    if user.login == auth.login and user.password == auth.password:
+        db[user.login] = user
+        data = {"admin": user.admin, "login": user.login}
+        return Response(
+            201, json.dumps(data).encode(), {"Content-Type": "application/json"}
+        )
+
+    admin = get_admin(auth)
+
+    if isinstance(admin, Response):
+        return admin
+
     db[user.login] = user
     data = {"admin": user.admin, "login": user.login}
     return Response(
@@ -44,6 +73,10 @@ async def create_user(
 @router.delete("/users/{user_login}")
 async def delete_user(
     user_login: Annotated[str, parsed(path("user_login"), str)],
+    auth: Annotated[
+        BasicHTTPAuthCredentials,
+        parsed(header("Authorization"), BasicHTTPAuthCredentials.parse),
+    ],
 ) -> Response:
     """
     Удаляет пользователя по логину.
@@ -51,18 +84,16 @@ async def delete_user(
     - 403, если пользователь не является администратором (удалять нельзя).
     - 204 No Content при успешном удалении.
     """
+    admin = get_admin(auth)
+
+    if isinstance(admin, Response):
+        return admin
+
     user = db.get(user_login)
     if user is None:
         return Response(
             404,
             json.dumps({"detail": "User not found"}).encode(),
-            {"Content-Type": "application/json"},
-        )
-
-    if not user.admin:
-        return Response(
-            403,
-            json.dumps({"detail": "Cannot delete admin user"}).encode(),
             {"Content-Type": "application/json"},
         )
 
@@ -73,12 +104,21 @@ async def delete_user(
 @router.get("/user/{user_login}")
 async def get_info_user(
     user_login: Annotated[str, parsed(path("user_login"), str)],
+    auth: Annotated[
+        BasicHTTPAuthCredentials,
+        parsed(header("Authorization"), BasicHTTPAuthCredentials.parse),
+    ],
 ) -> Response:
     """
     Предоставляет инфу о пользователе по логину
     - 404, если пользователь не найден.
     - 201 при успешной отправке информации о пользователе.
     """
+    admin = get_admin(auth)
+
+    if isinstance(admin, Response):
+        return admin
+
     user = db.get(user_login)
     if user is None:
         return Response(
@@ -93,10 +133,13 @@ async def get_info_user(
     )
 
 
-@router.put("/users/{admin_login}")
+@router.put("/users")
 async def put_info_user(
-    admin_login: Annotated[str, parsed(path("admin_login"), str)],
     maybe_user_to_delete: Annotated[User, parsed(body(), User.from_dict)],
+    auth: Annotated[
+        BasicHTTPAuthCredentials,
+        parsed(header("Authorization"), BasicHTTPAuthCredentials.parse),
+    ],
 ) -> Response:
     """
     Удаляет пользователя по логину.
@@ -104,23 +147,20 @@ async def put_info_user(
     - 403, если пользователь-админ  не является администратором (удалять нельзя).
     - 204 No Content при успешном изменнии инфы.
     """
-    admin = db.get(admin_login)
+    admin = get_admin(auth)
+
+    if isinstance(admin, Response):
+        return admin
+
     user_to_delete = db.get(maybe_user_to_delete.login)
-    if admin is None or user_to_delete is None:
+    if user_to_delete is None:
         return Response(
             404,
-            json.dumps({"detail": "Admin or user not found"}).encode(),
+            json.dumps({"detail": "User not found"}).encode(),
             {"Content-Type": "application/json"},
         )
 
-    if not admin.admin:
-        return Response(
-            403,
-            json.dumps({"detail": "User is not admin"}).encode(),
-            {"Content-Type": "application/json"},
-        )
-
-    db[user_to_delete.login] = user_to_delete
+    db[user_to_delete.login] = maybe_user_to_delete
     return Response(204, b"", {})
 
 
